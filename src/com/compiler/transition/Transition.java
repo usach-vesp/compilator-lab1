3230217e6273fa3c25d0899c9b840ec1baf1abb1
madrid16 <m.madrid16@gmail.com>
2016-05-25 17:27:29 -0300

    test wfor create AFD from two union closure

--- src/com/compiler/transition/Transition.java
+++ src/com/compiler/transition/Transition.java
@@ -4,16 +4,20 @@ import com.compiler.machine.Robot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Transition {
 
     ArrayList<ArrayList<Integer>> transitions;
     ArrayList<HashMap<String, ArrayList<Integer>>> newStates;
+    ArrayList<ArrayList<String>> afd;
 
     public Transition(){
         this.transitions = new ArrayList<ArrayList<Integer>>();
+        this.transitions.add(new ArrayList<Integer>(Arrays.asList(0)));
         this.newStates = new ArrayList<HashMap<String, ArrayList<Integer>>>();
+        this.afd = new ArrayList<ArrayList<String>>();
     }
 
     public ArrayList<ArrayList<Integer>> getTransitions() {
@@ -24,21 +28,42 @@ public class Transition {
         return newStates;
     }
 
-    public void turnToDeterministic(Robot robot, ArrayList<String> words){
-        this.searchFirstState(robot.getTransitions().get(0));
-        this.searchStates(robot, words, 0);
+    public ArrayList<ArrayList<String>> getAfd() {
+        return afd;
     }
 
-    public void searchFirstState(ArrayList<String> row){
-        ArrayList transition = new ArrayList(Arrays.asList(0));
+    public void turnToDeterministic(Robot robot, ArrayList<String> words){
+        this.searchFirstState(robot, 0);
+        this.searchStates(robot, words, 0);
+        this.turnToAFD(words, 0);
+    }
+
+    private void turnToAFD(ArrayList<String> words, int index){
+        String empty = new String(new char[this.transitions.size()]).replace("\0", "ø,");
+        afd.add(index, new ArrayList(Arrays.asList(empty.split(","))));
+        for (String letter : words){
+            if (!this.newStates.get(index).get(letter).isEmpty()){
+                afd.get(index).set(this.transitions.indexOf(this.newStates.get(index).get(letter)), letter);
+            }
+            this.transitions.indexOf(this.newStates.get(index).get(letter));
+        }
+        if (this.newStates.size() > index + 1){
+            this.turnToAFD(words, index + 1);
+        }
+    }
+
+    public void searchFirstState(Robot robot, int row){
         int index = 0;
-        for (String element : row){
+        for (String element : robot.getTransitions().get(row)){
             if (element.equals("ε")){
-                transition.add(index);
+                if (this.transitions.get(0).indexOf(index) == -1){
+                    this.transitions.get(0).add(index);
+                }
+                this.searchFirstState(robot, index);
             }
             index++;
         }
-        this.transitions.add(transition);
+        Collections.sort(this.transitions.get(0));
     }
 
     public void searchStates(Robot robot, ArrayList<String> words, int index){
@@ -88,6 +113,7 @@ public class Transition {
     }
 
     public void validateTransition(ArrayList<Integer> finalTransition){
+        Collections.sort(finalTransition);
         if (this.transitions.indexOf(finalTransition) == -1 && finalTransition.size() > 0){
             this.transitions.add(finalTransition);
         }
--- src/test/com/compiler/junit/TransitionTest.java
+++ src/test/com/compiler/junit/TransitionTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -36,22 +35,92 @@ public class TransitionTest {
     @Test
     public void turnToDeterministic() throws Exception {
         transition.turnToDeterministic(robot, new ArrayList<String>(Arrays.asList("a", "b")));
+        assertEquals(transition.getAfd().size(), 3);
+        assertEquals(transition.getAfd().get(0).size(), 3);
+        assertEquals(transition.getAfd().get(0).get(0), "ø");
+        assertEquals(transition.getAfd().get(0).get(1), "a");
+        assertEquals(transition.getAfd().get(0).get(2), "b");
+        assertEquals(transition.getAfd().get(1).size(), 3);
+        assertEquals(transition.getAfd().get(1).get(0), "ø");
+        assertEquals(transition.getAfd().get(1).get(1), "ø");
+        assertEquals(transition.getAfd().get(1).get(2), "ø");
+        assertEquals(transition.getAfd().get(2).size(), 3);
+        assertEquals(transition.getAfd().get(2).get(0), "ø");
+        assertEquals(transition.getAfd().get(2).get(1), "ø");
+        assertEquals(transition.getAfd().get(2).get(2), "ø");
+    }
+
+    @Test
+    public void turnToDeterministicThreeUnion() throws Exception {
+        Robot robot = new Robot();
+        ArrayList<Robot> machines = new ArrayList<Robot>(Arrays.asList(generateMachine.base("a"),
+                generateMachine.base("b"), generateMachine.base("c")));
+        robot = generateMachine.union(machines);
+        transition.turnToDeterministic(robot, new ArrayList<String>(Arrays.asList("a", "b", "c")));
+        assertEquals(transition.getAfd().size(), 4);
+        assertEquals(transition.getAfd().get(0).size(), 4);
+        assertEquals(transition.getAfd().get(0).get(0), "ø");
+        assertEquals(transition.getAfd().get(0).get(1), "a");
+        assertEquals(transition.getAfd().get(0).get(2), "b");
+        assertEquals(transition.getAfd().get(0).get(3), "c");
+        assertEquals(transition.getAfd().get(1).size(), 4);
+        assertEquals(transition.getAfd().get(1).get(0), "ø");
+        assertEquals(transition.getAfd().get(1).get(1), "ø");
+        assertEquals(transition.getAfd().get(1).get(2), "ø");
+        assertEquals(transition.getAfd().get(1).get(3), "ø");
+        assertEquals(transition.getAfd().get(2).size(), 4);
+        assertEquals(transition.getAfd().get(2).get(0), "ø");
+        assertEquals(transition.getAfd().get(2).get(1), "ø");
+        assertEquals(transition.getAfd().get(2).get(2), "ø");
+        assertEquals(transition.getAfd().get(2).get(3), "ø");
+    }
+
+    @Test
+    public void turnToDeterministicTwoUnionClosure() throws Exception {
+        Robot robot;
+        ArrayList<Robot> machines = new ArrayList<Robot>(Arrays.asList(
+                generateMachine.closure(generateMachine.base("a")),
+                generateMachine.closure(generateMachine.base("b"))));
+        robot = generateMachine.union(machines);
+        transition.turnToDeterministic(robot, new ArrayList<String>(Arrays.asList("a", "b")));
+        assertEquals(transition.getAfd().size(), 3);
+        assertEquals(transition.getAfd().get(0).size(), 3);
+        assertEquals(transition.getAfd().get(0).get(0), "ø");
+        assertEquals(transition.getAfd().get(0).get(1), "a");
+        assertEquals(transition.getAfd().get(0).get(2), "b");
+        assertEquals(transition.getAfd().get(1).size(), 3);
+        assertEquals(transition.getAfd().get(1).get(0), "ø");
+        assertEquals(transition.getAfd().get(1).get(1), "a");
+        assertEquals(transition.getAfd().get(1).get(2), "ø");
+        assertEquals(transition.getAfd().get(2).size(), 3);
+        assertEquals(transition.getAfd().get(2).get(0), "ø");
+        assertEquals(transition.getAfd().get(2).get(1), "ø");
+        assertEquals(transition.getAfd().get(2).get(2), "b");
     }
 
     @Test
     public void searchFirstState() throws Exception {
-        ArrayList arrayList = new ArrayList(Arrays.asList("ø", "ε", "ø", "ε", "ø", "ø"));
-        transition.searchFirstState(arrayList);
-        assertEquals(transition.getTransitions().size(), 3);
-        assertEquals(transition.getTransitions().get(0), 0);
-        assertEquals(transition.getTransitions().get(1), 1);
-        assertEquals(transition.getTransitions().get(2), 3);
+        Robot robot;
+        ArrayList<Robot> machines = new ArrayList<Robot>(Arrays.asList(
+                generateMachine.closure(generateMachine.base("a")),
+                generateMachine.closure(generateMachine.base("b"))));
+        robot = generateMachine.union(machines);
+        transition.searchFirstState(robot, 0);
+        assertEquals(transition.getTransitions().get(0).size(), 8);
+        assertEquals(transition.getTransitions().get(0), new ArrayList<Integer>(Arrays.asList(0, 1, 2, 4, 5, 6, 8, 9)));
+    }
+
+    @Test
+    public void searchFirstStateTwoClosure() throws Exception {
+        transition.searchFirstState(robot, 0);
+        assertEquals(transition.getTransitions().get(0).size(), 3);
+        assertEquals(transition.getTransitions().get(0), new ArrayList<Integer>(Arrays.asList(0, 1, 3)));
     }
 
     @Test
     public void searchStates() throws Exception{
         ArrayList<String> words = new ArrayList<>(Arrays.asList("a", "b"));
-        transition.searchFirstState(robot.getTransitions().get(0));
+        transition.searchFirstState(robot, 0);
         transition.searchStates(robot, words, 0);
         assertEquals(transition.getTransitions().size(), 3);
         assertEquals(transition.getTransitions().get(0), new ArrayList<Integer>(Arrays.asList(0, 1, 3)));
@@ -68,7 +137,7 @@ public class TransitionTest {
 
     @Test
     public void getNewTransitions() throws Exception{
-        transition.searchFirstState(new ArrayList<String>(Arrays.asList("ø", "ε", "ø", "ε", "ø", "ø")));
+        transition.searchFirstState(robot, 0);
         transition.getNewTransitions(robot, new ArrayList<String>(Arrays.asList("a", "b")), 0);
         assertEquals(transition.getTransitions().size(), 3);
         assertEquals(transition.getTransitions().get(0), new ArrayList<Integer>(Arrays.asList(0, 1, 3)));
@@ -97,9 +166,9 @@ public class TransitionTest {
     public void validateTransition() throws Exception{
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(0, 1));
         transition.validateTransition(arrayList);
-        assertEquals(transition.getTransitions().size(), 1);
-        assertEquals(transition.getTransitions().get(0).size(), 2);
-        assertEquals(transition.getTransitions().get(0), Arrays.asList(0, 1));
+        assertEquals(transition.getTransitions().size(), 2);
+        assertEquals(transition.getTransitions().get(0).size(), 1);
+        assertEquals(transition.getTransitions().get(0), Arrays.asList(0));
     }
 
 }
